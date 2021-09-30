12e1b07428aa7ea2206c04f2a0c89e91477e3c27
Marcos <marcosugaldemartinez@gmail.com>
2021-09-30 07:21:58 +0200

    added Pokecard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Pokecard from "./Pokecard";
 
 function App() {
   const [allPokemons, setAllPokemons] = useState([]);
@@ -7,15 +8,30 @@ function App() {
     "https://pokeapi.co/api/v2/pokemon?limit-20"
   );
 
-  const getAllPokemons = async () => {
+  const getAllPokemon = async () => {
     const res = await axios.get(loadMore);
-    console.log(res.data);
     setLoadMore(res.data.next);
+
+    const createPokemonObject = (result) => {
+      result.forEach(async (pokemon) => {
+        const res = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        setAllPokemons((currentList) => [...currentList, res.data]);
+      });
+    };
+    createPokemonObject(res.data.results);
+    console.log(allPokemons);
   };
+
+  useEffect(() => {
+    getAllPokemon();
+  }, []);
+
   return (
-    <div>
+    <div className="app-container">
       <h1>Pokemon</h1>
-      <button onClick={getAllPokemons}>Load More</button>
+      <button onClick={getAllPokemon}>Load More!</button>
     </div>
   );
 }
